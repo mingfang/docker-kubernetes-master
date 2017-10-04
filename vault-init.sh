a026afa941fb18a3b91b73ec5c2749ed00da5c0e
Ming Fang <mingfang@mac.com>
2017-10-04 01:15:53 -0400

    use vault

---
+++ vault-init.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+vault server -config=/vault.hcl&
+sleep 3
+VAULT_DATA=/var/lib/vault-data
+
+if ! vault init -check; then
+    echo "Initializing Vault..."
+    vault init > $VAULT_DATA/VAULT_INIT
+fi
+
+#Unseal
+grep "Unseal Key" $VAULT_DATA/VAULT_INIT | awk '{print $4}' | xargs -I {} vault unseal {}
+
+#auth as root
+grep "Root Token" $VAULT_DATA/VAULT_INIT | awk '{print $4}' | xargs -I {} vault auth {}
+
+#init CA
+vault mount -path kubernetes pki
+vault mount-tune -max-lease-ttl=87600h kubernetes
+
+#generate root certificate
+vault write kubernetes/root/generate/internal common_name=kubernetes ttl=87600h
+
+#api-server pki role
+vault write kubernetes/roles/kube-apiserver allow_any_name=true enforce_hostnames=false max_ttl="720h"
+
+#kubelet pki role
+vault write kubernetes/roles/kubelet allow_any_name=true enforce_hostnames=false organization="system:nodes" max_ttl="720h"
+
+#kube-proxy pki role
+vault write kubernetes/roles/kube-proxy allow_any_name=true enforce_hostnames=false max_ttl="720h"
+
+#kube-apiserver vault policy
+cat <<EOT | vault policy-write kubernetes/policy/kube-apiserver -
+path "kubernetes/issue/kube-apiserver" {
+  policy = "write"
+}
+path "secret/kubernetes/service-account-key" {
+  policy = "read"
+}
+EOT
+
+#kubelet vault policy
+cat <<EOT | vault policy-write kubernetes/policy/kubelet -
+path "kubernetes/issue/kubelet" {
+  policy = "write"
+}
+path "secret/kubernetes/service-account-key" {
+  policy = "read"
+}
+EOT
+
+#kube-proxy vault policy
+cat <<EOT | vault policy-write kubernetes/policy/kube-proxy -
+path "kubernetes/issue/kube-proxy" {
+  policy = "write"
+}
+path "secret/kubernetes/service-account-key" {
+  policy = "read"
+}
+EOT
+
+#kube-apiserver auth role
+vault write auth/token/roles/kube-apiserver period="720h" orphan=true allowed_policies="kubernetes/policy/kube-apiserver"
+
+#kubelet auth role
+vault write auth/token/roles/kubelet period="720h" orphan=true allowed_policies="kubernetes/policy/kubelet"
+
+#kube-proxy auth role
+vault write auth/token/roles/kube-proxy period="720h" orphan=true allowed_policies="kubernetes/policy/kube-proxy"
+
+#service account secret key
+#openssl genrsa 4096 > $VAULT_DATA/service-account-key
+#vault write secret/kubernetes/service-account-key key=@$VAULT_DATA/service-account-key
+#rm $VAULT_DATA/service-account-key
+
+#setup token for kmaster
+vault token-create -role="kube-apiserver" > /dev/shm/KMASTER_TOKEN
+
---
+++ vault.hcl
@@ -0,0 +1,10 @@
+disable_mlock = true
+
+backend "file" {
+  path = "/var/lib/vault-data"
+}
+
+listener "tcp" {
+  address = "0.0.0.0:8200"
+  tls_disable = 1
+}
