13e04fe840a5689226ee1ca8080de12b0a52056c
Ming Fang <mingfang@mac.com>
2017-10-09 18:45:17 -0400

    enable vault aws integration

--- vault-init.sh
+++ vault-init.sh
@@ -14,6 +14,7 @@ grep "Unseal Key" $VAULT_DATA/VAULT_INIT | awk '{print $4}' | xargs -I {} vault 
 
 #auth as root
 grep "Root Token" $VAULT_DATA/VAULT_INIT | awk '{print $4}' | xargs -I {} vault auth {}
+vault audit-enable file file_path=/var/log/vault_audit.log
 
 #init CA
 vault mount -path kubernetes pki
@@ -30,35 +31,6 @@ vault write kubernetes/roles/kubelet allow_any_name=true enforce_hostnames=false
 
 #kube-proxy pki role
 vault write kubernetes/roles/kube-proxy allow_any_name=true enforce_hostnames=false max_ttl="720h"
-
-#enable AWS integration
-vault auth-enable aws
-
-#vault aws policy
-cat <<EOT | vault policy-write aws/policy/vault -
-path "secret/aws/*" {
-  policy = "write"
-}
-path "auth/aws/login" {
-  policy = "write"
-}
-EOT
-
-#vault aws role
-vault write auth/aws/role/vault auth_type=ec2 policies="aws/policy/vault"
-
-#ec2 instance aws policy
-cat <<EOT | vault policy-write aws/policy/ec2 -
-path "secret/aws/*" {
-  policy = "write"
-}
-path "auth/aws/login" {
-  policy = "write"
-}
-EOT
-
-#ec2 aws role
-vault write auth/aws/role/ec2 auth_type=ec2 policies="aws/policy/ec2"
 
 #kube-apiserver vault policy
 cat <<EOT | vault policy-write kubernetes/policy/kube-apiserver -
@@ -104,6 +76,23 @@ vault write auth/token/roles/kube-proxy period="720h" orphan=true allowed_polici
 #openssl genrsa 4096 > $VAULT_DATA/service-account-key
 #vault write secret/kubernetes/service-account-key key=@$VAULT_DATA/service-account-key
 #rm $VAULT_DATA/service-account-key
+
+if $VPC_ID; then
+#enable AWS integration
+vault auth-enable aws
+
+#vault aws policy
+cat <<EOT | vault policy-write aws/policy/knode -
+path "secret/aws/*" {
+  policy = "write"
+}
+path "auth/aws/login" {
+  policy = "write"
+}
+EOT
+#knode aws role
+vault write auth/aws/role/knode auth_type=ec2 bound_vpc_id="$VPC_ID" policies="aws/policy/knode,kubernetes/policy/kubelet,kubernetes/policy/kube-proxy"
+fi
 
 #setup token for kmaster
 vault token-create -role="kube-apiserver" > /dev/shm/KMASTER_TOKEN
