49f268f424f5e627e5a38df5404d7810503a6f82
root <mingfang@mac.com>
2018-12-03 02:07:38 -0500

    upgrade helper

--- etc/kubernetes/addons/update-addons.sh
+++ etc/kubernetes/addons/update-addons.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+wget --backups=1 https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/metrics-server/auth-delegator.yaml
+wget --backups=1 https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/metrics-server/auth-reader.yaml
+wget --backups=1 https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/metrics-server/metrics-apiservice.yaml
+wget --backups=1 https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/metrics-server/metrics-server-deployment.yaml
+wget --backups=1 https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/metrics-server/metrics-server-service.yaml
+wget --backups=1 https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/metrics-server/resource-reader.yaml
+
+wget --backups=1 https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/dns-horizontal-autoscaler/dns-horizontal-autoscaler.yaml
+
+wget https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/dns/coredns/coredns.yaml.sed -O coredns.yaml
+
+rm *1
